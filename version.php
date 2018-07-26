d1e95be357220b5da908e042655f00a1ec2841ee
Luuk Verhoeven <luuk@ldesign.nl>
2018-07-26 09:53:56 +0200

    Fix deleted user update avatar
    We can't update deleted users there avatar.

--- version.php
+++ version.php
@@ -27,6 +27,6 @@
 defined('MOODLE_INTERNAL') || die();
 $plugin->release   = '1.2.0';
 $plugin->maturity = MATURITY_STABLE;
-$plugin->version   = 2018060703;        // The current plugin version (Date: YYYYMMDDXX)
+$plugin->version   = 2018072600;        // The current plugin version (Date: YYYYMMDDXX)
 $plugin->requires  = 2018051700;        // Requires this Moodle 3.5 version
 $plugin->component = 'block_mfavatar'; // Full name of the plugin (used for diagnostics)
